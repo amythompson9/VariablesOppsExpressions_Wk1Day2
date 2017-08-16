a4712e4488050445f8c82caad667833f4adc808e
amythompson9 <amythompson9@gmail.com>
2017-08-15 20:50:35 -0400

    Created more variable types

--- VariablesOppsExpressions_Wk1Day2/Program.cs
+++ VariablesOppsExpressions_Wk1Day2/Program.cs
@@ -10,6 +10,41 @@ namespace VariablesOppsExpressions_Wk1Day2
     {
         static void Main(string[] args)
         {
+            string name = "Amy";
+            //Console.WriteLine(name);
+
+            name = "Dave";
+            //Console.WriteLine(name);
+            string Name = "Amy";
+            string birthMonth = "March";
+            //Console.WriteLine(birthMonth);
+            //integers
+            int birthMonthNumber = 3;
+            int age = 65;
+            int population = 100000;
+            //floating type variables
+            //float number = 2.164403304897294383961032f;
+            //double someNumber = 2.164403304897294383961032d;
+            //decimal partialNumber = 2.164403304897294383961032m;
+
+            //Console.WriteLine(number);
+            //Console.WriteLine(someNumber);
+            //Console.WriteLine(partialNumber);
+
+
+
+
+            //Boolean
+            bool isPresent = true;
+            bool seatTaken = false;
+            //Character
+            char lastLetter = 'j';
+
+            int jessicaAge = 23;
+            int samAge = 47;
+            Console.WriteLine(jessicaAge > samAge);
+
+
         }
     }
 }
